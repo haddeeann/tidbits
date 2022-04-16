10011d9de0d1aa61bc211170f511a1ae8e41bfbe
HaddeeAnn <haddeeann@HaddeeAnns-MBP.attlocal.net>
2022-04-15 22:08:41 -0400

    some work on long pal

--- leetcode/python/5/long_pal.py
+++ leetcode/python/5/long_pal.py
@@ -1,47 +1,71 @@
+import math
+
+
 class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        pal = ""
-        longest = 0
-        if len(s) == "":
-            return ""
-        if len(s) == 1:
-            return s
-        if len(s) == 2 and s[0] != s[1]:
-            return s[0]
+        pal = s[0]
+        longest = 1
+        s_len = len(s)
+
+        def testPalindrome(p):
+            if len(p) % 2 == 0:
+                # abcd
+                i_right = int(len(p) / 2)
+                i_left = i_right - 1
+            else:
+                # abc
+                i = int(math.floor(len(p) / 2))
+                i_right = i + 1
+                i_left = i - 1
+
+            while i_left >= 0 and i_right < len(p):
+                if p[i_left] != p[i_right]:
+                    return False
+                i_left -= 1
+                i_right += 1
+
+            return True
 
         for i, letter in enumerate(s):
-            word = letter
-            lt_i = i - 1
-            gt_i = i + 1
-            if lt_i >= 0:
-                if letter == s[lt_i]:
-                    if (gt_i < len(s) and s[gt_i] != letter) or gt_i >= len(s) or (gt_i < len(s) and s[gt_i] == letter and len(s) % 2 == 0):
-                        word = s[lt_i] + word
-                        if len(word) > longest:
-                            longest = len(word)
-                            pal = word
-                        lt_i -= 1
+            j = i - 1
+            k = i + 1
+            word = s[i]
+            while j >= 0 and k < s_len:
+                word = s[j] + word + s[k]
+                if testPalindrome(word) and len(word) > longest:
+                    longest = len(word)
+                    pal = word
+                j -= 1
+                k += 1
 
-            # main loop
-            while lt_i >= 0 and gt_i < len(s):
-                if s[lt_i] == s[gt_i]:
-                    word = s[lt_i] + word + s[gt_i]
-                    lt_i -= 1
-                    gt_i += 1
-                    if len(word) > longest:
-                        longest = len(word)
-                        pal = word
-                else:
-                    break
+            if i > 0:
+                j = i - 2
+                k = i + 1
+                word = s[i-1] + s[i]
+            else:
+                j = i - 1
+                k = i + 1
+                word = s[i]
 
-        if len(s) > 0 and len(pal) == 0:
-            return s[0]
+            if testPalindrome(word) and len(word) > longest:
+                longest = len(word)
+                pal = word
+            while j >= 0 and k < s_len:
+                word = s[j] + word + s[k]
+                if testPalindrome(word) and len(word) > longest:
+                    longest = len(word)
+                    pal = word
+                j -= 1
+                k += 1
+
         return pal
 
 
 sol = Solution()
-print(sol.longestPalindrome("aaaa"))
+sol.longestPalindrome(
+    "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa")
+# print(sol.longestPalindrome("azwdzwmwcqzgcobeeiphemqbjtxzwkhiqpbrprocbppbxrnsxnwgikiaqutwpftbiinlnpyqstkiqzbggcsdzzjbrkfmhgtnbujzszxsycmvipjtktpebaafycngqasbbhxaeawwmkjcziybxowkaibqnndcjbsoehtamhspnidjylyisiaewmypfyiqtwlmejkpzlieolfdjnxntonnzfgcqlcfpoxcwqctalwrgwhvqvtrpwemxhirpgizjffqgntsmvzldpjfijdncexbwtxnmbnoykxshkqbounzrewkpqjxocvaufnhunsmsazgibxedtopnccriwcfzeomsrrangufkjfzipkmwfbmkarnyyrgdsooosgqlkzvorrrsaveuoxjeajvbdpgxlcrtqomliphnlehgrzgwujogxteyulphhuhwyoyvcxqatfkboahfqhjgujcaapoyqtsdqfwnijlkknuralezqmcryvkankszmzpgqutojoyzsnyfwsyeqqzrlhzbc"))
