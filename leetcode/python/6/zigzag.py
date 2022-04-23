0977122ffd1551bc5cb3f74738124b6bf9aefe93
HaddeeAnn <haddeeann@HaddeeAnns-MacBook-Pro.local>
2022-04-22 20:07:20 -0400

    ready to make a string

--- leetcode/python/6/zigzag.py
+++ leetcode/python/6/zigzag.py
@@ -15,77 +15,27 @@ class Solution(object):
         arr = []
         z = 0
         i = 0
-        # pattern one
-        self.fill(numRows, arr)
-        y = 0
-        while y < numRows:
-            arr[i][y] = s[z]
-            y += 1
-            z += 1
 
-        # pattern two
-        i += 1
-        j = 3
-        while j > 0:
+        s_length = len(s)
+
+        while z < s_length:
+            # pattern one
             self.fill(numRows, arr)
-            arr[i][j] = s[z]
+            y = 0
+            while y < numRows:
+                arr[i][y] = s[z]
+                y += 1
+                z += 1
+
+            # pattern two
             i += 1
-            j -= 1
-            z += 1
-
-        # pattern one
-        self.fill(numRows, arr)
-        y = 0
-        while y < numRows:
-            arr[i][y] = s[z]
-            y += 1
-            z += 1
-
-        # pattern two
-        i += 1
-        j = 3
-        while j > 0:
-            self.fill(numRows, arr)
-            arr[i][j] = s[z]
-            i += 1
-            j -= 1
-            z += 1
-
-        # pattern one
-        self.fill(numRows, arr)
-        y = 0
-        while y < numRows:
-            arr[i][y] = s[z]
-            y += 1
-            z += 1
-
-        # pattern two
-        i += 1
-        j = 3
-        while j > 0:
-            self.fill(numRows, arr)
-            arr[i][j] = s[z]
-            i += 1
-            j -= 1
-            z += 1
-
-        # pattern one
-        self.fill(numRows, arr)
-        y = 0
-        while y < numRows:
-            arr[i][y] = s[z]
-            y += 1
-            z += 1
-
-        # pattern two
-        i += 1
-        j = 3
-        while j > 0:
-            self.fill(numRows, arr)
-            arr[i][j] = s[z]
-            i += 1
-            j -= 1
-            z += 1
+            j = 3
+            while j > 0:
+                self.fill(numRows, arr)
+                arr[i][j] = s[z]
+                i += 1
+                j -= 1
+                z += 1
 
         for index, a in enumerate(arr):
             print(a)
