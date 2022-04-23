34ecd817a86dc6dec9d58b86656a1e976fcf3605
HaddeeAnn <haddeeann@HaddeeAnns-MBP.attlocal.net>
2022-04-22 22:38:54 -0400

    start of reverse int

--- leetcode/python/7/reverse_int.py
+++ leetcode/python/7/reverse_int.py
@@ -4,3 +4,20 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        random_a = 4
+        random_b = 5
+        add_3 = random_a & random_b
+        # lower_limit = -(2 ** 31)
+        # upper_limit = (2 ** 31) - 1
+        print("{0:b}".format(random_a))
+        print("{0:b}".format(random_b))
+        print("{0:b}".format(add_3))
+
+        if add_3 == random_a and add_3 == random_b:
+            print("same")
+        else:
+            print(add_3)
+
+
+sol = Solution()
+sol.reverse(123)
