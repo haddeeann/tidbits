d20229cb508990011b15051643461300cd5cac29
HaddeeAnn <haddeeann@HaddeeAnns-MBP.attlocal.net>
2022-06-05 21:35:22 -0400

    move zeroes

--- leetcode/python/283/move_zeroes.py
+++ leetcode/python/283/move_zeroes.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        zeros = nums.count(0)
+        for x in range(zeros):
+            nums.remove(0)
+
+        for x in range(zeros):
+            nums.append(0)
+
+        print(nums)
+
+
+sol = Solution()
+nums = [0, 1, 0, 3, 12]
+sol.moveZeroes(nums)
