a976e4b8c98fdf3e696f3df165e02bdc8d21eb28
HaddeeAnn <haddeeann@HaddeeAnns-MBP.attlocal.net>
2022-07-31 17:46:25 -0400

    finished longest prefix, and added three sum

--- leetcode/python/14/longest_prefix.py
+++ leetcode/python/14/longest_prefix.py
@@ -4,25 +4,41 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        short = True
-        count = 0
-        test_value = len(strs[1]) - 1
-        while short:
-            for x in strs:
-                print(x[count])
-            count += 1
-            print('hi, good looking')
-            if count == test_value:
-                short = False
-        #["flower","flow","flight"]
-        # print(strs[0][0])
-        # print(strs[1][0])
-        # print(strs[2][0])
-        #
-        # print(strs[0][1])
-        # print(strs[1][1])
-        # print(strs[2][1])
+        pattern = ''
+        word_1 = ''
+        word_2 = ''
+        overall_min_length = None
+        word_pattern = ''
+        if len(strs) == 1:
+            return strs[0]
+        for i, word in enumerate(strs):
+            if i != len(strs) - 1:
+                word_1 = strs[i]
+                word_2 = strs[i+1]
+                short_word_length = min(len(word_1), len(word_2))
+                if overall_min_length is None:
+                    overall_min_length = short_word_length
+                else:
+                    overall_min_length = min(overall_min_length, short_word_length)
+                if pattern == '' and i == 0:
+                    for x in range(short_word_length):
+                        if word_1[x] == word_2[x]:
+                            pattern += word_1[x]
+                        else:
+                            break
+                elif pattern != '':
+                    overall_min_length = min(overall_min_length, len(pattern))
+                    word_pattern = ''
+                    for x in range(overall_min_length):
+                        if word_1[x] == word_2[x] == pattern[x]:
+                            word_pattern += word_1[x]
+                        else:
+                            break
 
+                    if len(word_pattern) < len(pattern):
+                        pattern = word_pattern
+
+        return pattern
 
 sol = Solution()
-sol.longestCommonPrefix(["flower","flow","flight"])
+print(sol.longestCommonPrefix(["a","aca","accb","b"]))
