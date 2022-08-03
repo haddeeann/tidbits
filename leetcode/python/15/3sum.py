2d307fe5a1374de6d434a2b09ed0f6752b0d6bd3
HaddeeAnn <haddeeann@HaddeeAnns-MBP.attlocal.net>
2022-08-02 20:34:13 -0400

    three sum and added start for three sum closest

--- leetcode/python/15/3sum.py
+++ leetcode/python/15/3sum.py
@@ -4,4 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        answer_list = []
+        nums.sort()
         print(nums)
+        for index, test_num in enumerate(nums[:-2]):
+            if index != 0 and test_num == nums[index-1]:
+                continue
+            left_index = index + 1
+            right_index = len(nums) - 1
+            while left_index < right_index:
+                left = nums[left_index]
+                right = nums[right_index]
+                sum = test_num + left + right
+                if sum > 0:
+                    right_index -= 1
+                elif sum < 0:
+                    left_index += 1
+                elif sum == 0:
+                    answer_list.append([test_num, nums[left_index], nums[right_index]])
+                    while left == nums[left_index] and left_index < right_index:
+                        left_index += 1
+
+        return answer_list
+
+
+sol = Solution()
+print(sol.threeSum([-1,0,1,2,-1,-4]))
---
+++ leetcode/python/16/three_sum_closest.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
