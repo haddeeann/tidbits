9d62825f2d362368e858a3c829f894d19db5d8a7
HaddeeAnn <haddeeann@HaddeeAnns-MBP.attlocal.net>
2022-08-09 21:41:08 -0400

    three sum closest first try

--- leetcode/python/16/three_sum_closest.py
+++ leetcode/python/16/three_sum_closest.py
@@ -5,3 +5,37 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        closest = 0
+        least_diff = None
+        test_diff = 0
+        nums.sort()
+
+        for l_index, left in enumerate(nums[:-3]):
+            r_index = len(nums) - 1
+            m_index = l_index + 1
+
+            while m_index < r_index:
+                right = nums[r_index]
+                middle = nums[m_index]
+                print(left, middle, right)
+                sum = left + middle + right
+                test_diff = abs(sum - target)
+                if least_diff is None or test_diff < least_diff:
+                    least_diff = test_diff
+
+                if sum > target:
+                    # if sum is too big, move right to be decrease
+                    r_index -= 1
+                elif sum < target:
+                    # if sum too small, move middle index to increase
+                    m_index += 1
+                elif sum == target:
+                    return target
+
+        return least_diff
+
+
+
+
+sol = Solution()
+sol.threeSumClosest([-1,2,1,-4], 1)
