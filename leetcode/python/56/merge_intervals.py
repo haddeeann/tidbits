c9a49a6bde9001b7784bf14f240511b2549a4d11
HaddeeAndd <haddeeann@gmail.com>
2022-08-30 21:06:01 -0400

    better, but still needs work for merge intervals

--- leetcode/python/56/merge_intervals.py
+++ leetcode/python/56/merge_intervals.py
@@ -6,28 +6,39 @@ class Solution(object):
         """
         result_list = []
         overlap = None
-        def test_small(small, t_1, t_2):
-            overlap = False
-            first_small = small[0]
-            second_small = small[1]
-            if first_small <= t_1 <= second_small:
-                overlap = True
-            if first_small <= t_2 <= second_small:
-                overlap = True
 
-            return overlap
+        def sort_intervals(e):
+            return e[0]
+
+        intervals.sort(key=sort_intervals)
+
+        def test_small(small_arr, t_1, t_2):
+            overlap_test = False
+            first_small = small_arr[0]
+            second_small = small_arr[1]
+            if first_small <= t_1 <= second_small:
+                overlap_test = True
+            if first_small <= t_2 <= second_small:
+                overlap_test = True
+
+            return overlap_test
 
         for i, small in enumerate(intervals):
             if overlap:
                 overlap = False
                 continue
             if i == len(intervals) - 1:
+                # if last one then just add, if we haven't skipped last due to overlap
                 result_list.append(small)
             else:
                 next_small = intervals[i+1]
+                # test overlap
                 overlap = test_small(small, next_small[0], next_small[1])
                 if overlap:
-                    result_list.append([small[0], next_small[1]])
+                    # add the correct overlap
+                    smallest_left = min(small[0], next_small[0])
+                    largest_right = max(small[1], next_small[1])
+                    result_list.append([smallest_left, largest_right])
                 else:
                     result_list.append(small)
         print(result_list)
@@ -35,23 +46,34 @@ class Solution(object):
 
 
 sol = Solution()
-intervals = [[1,3],[2,6],[8,10],[15,18]]
-result = sol.merge(intervals)
-answer = [[1,6],[8,10],[15,18]]
+backup_tests = [
+    {
+        "no": "first",
+        "interval": [[1, 3], [2, 6], [8, 10], [15, 18]],
+        "answer": [[1, 6], [8, 10], [15, 18]]
+    },
+    {
+        "no": "second",
+        "interval": [[1, 4], [4, 5]],
+        "answer": [[1, 5]]
+    },
+    {
+        "no": "third",
+        "interval": [[1, 4], [0, 4]],
+        "answer": [[0, 4]]
+    }
+]
+test_set = [
+    {
+        "no": "fourth",
+        "interval": [[1,4],[0,5]],
+        "answer": [[0,5]]
+    }
+]
 
-if result == answer:
-    print("first answer is right")
-else:
-    print("first answer is wrong")
-
-intervals = [[1,4],[4,5]]
-result = sol.merge(intervals)
-answer = [[1,5]]
-
-if result == answer:
-    print("second answer is right")
-else:
-    print("second answer is wrong")
-
-# [[1,4],[0,4]]
-# [[0,4]]
+for test in test_set:
+    result = sol.merge(test['interval'])
+    if result == test['answer']:
+        print(f"{test['no']} answer is right")
+    else:
+        print(f"{test['no']} answer is wrong")
