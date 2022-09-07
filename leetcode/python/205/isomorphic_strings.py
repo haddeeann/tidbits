4adab9679adfe7af23fecd178ba05cac2d9f1f13
HaddeeAndd <haddeeann@gmail.com>
2022-09-06 20:52:59 -0400

    is isomorphic is finished

--- leetcode/python/205/isomorphic_strings.py
+++ leetcode/python/205/isomorphic_strings.py
@@ -5,7 +5,21 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        pass
+        t_dict = {}
+        s_dict = {}
+        for i, l in enumerate(s):
+            if t[i] not in t_dict:
+                t_dict[t[i]] = l
+            else:
+                if t_dict[t[i]] != l:
+                    return False
+            if l not in s_dict:
+                s_dict[l] = t[i]
+            else:
+                if s_dict[l] != t[i]:
+                    return False
+
+        return True
 
 
 sol = Solution()
@@ -15,13 +29,33 @@ t = [
         "no": "one",
         "i_one": "egg",
         "i_two": "add",
-        "answer": None
+        "answer": True
+    },
+    {
+        "no": "two",
+        "i_one": "foo",
+        "i_two": "bar",
+        "answer": False
+    },
+    {
+        "no": "three",
+        "i_one": "paper",
+        "i_two": "title",
+        "answer": True
+    },
+    {
+        "no": "four",
+        "i_one": "badc",
+        "i_two": "baba",
+        "answer": False
     }
 ]
+s = [
 
-for x in test_set:
-    a = sol.isIsomorphic(t["i_one"], t["i_two"])
-    if a == t["answer"]:
-        print(f"{no}: answer is right")
+]
+for x in t:
+    a = sol.isIsomorphic(x["i_one"], x["i_two"])
+    if a == x["answer"]:
+        print(f"{x['no']}: answer is right")
     else:
-        print(f"{no}: test has FAILED")
+        print(f"{x['no']}: test has FAILED")
