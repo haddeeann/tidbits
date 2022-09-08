85e69c6f11bbe5b86668ca3399275281c7cac221
HaddeeAndd <haddeeann@gmail.com>
2022-09-07 21:49:45 -0400

    added fizzbuzz and worked on sub

---
+++ leetcode/python/392/sub.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        r = ""
+        i = 0
+        last_s = len(s) - 1
+        for x in t:
+            if i < last_s and x == s[i]:
+                r += x
+                i += 1
+
+        return r == s
+
+
+sol = Solution()
+test_set = [
+    {
+        "no": "one",
+        "input_1": "abc",
+        "input_2": "ahbgdc",
+        "answer": True
+    },
+    {
+        "no": "two",
+        "input_1": "",
+        "input_2": "ahbgdc",
+        "answer": True
+    },
+]
+for x in test_set:
+    result = sol.isSubsequence(x["input_1"], x["input_2"])
+    if result == x["answer"]:
+        print("correct")
+    else:
+        print("FAILED")
---
+++ leetcode/python/current.md
@@ -0,0 +1,1 @@
+working on subsequence #392 and fizzbuzz #412
