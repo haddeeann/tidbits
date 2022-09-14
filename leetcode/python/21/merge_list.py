37afdd1243abc268622391bbddd8a4afaaca2d9a
HaddeeAndd <haddeeann@gmail.com>
2022-09-13 20:59:30 -0400

    transverse the list

--- leetcode/python/21/merge_list.py
+++ leetcode/python/21/merge_list.py
@@ -12,8 +12,16 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        while list1.next and list2.next:
-            print(list1.val)
+        node_a = list1
+        node_b = list2
+        while node_a or node_b:
+            if node_a:
+                print(node_a.val)
+                node_a = node_a.next
+            elif node_b:
+                print(node_b.val)
+                node_b = node_b.next
+
 
 
 # https://stackoverflow.com/questions/71569455/traverse-listnode-in-python
@@ -26,34 +34,35 @@ def createLinkedList(values):
 
 
 sol = Solution()
-list_node_1 = ListNode()
-l_1 = createLinkedList([1, 2, 4])
-print(l_1)
 
 test_cases = [
     {
         "no": "one",
-        "input_1": ListNode([1, 2, 4]),
-        "input_2": ListNode([1, 3, 4]),
-        "answer": ListNode([1, 1, 2, 3, 4, 4])
-    },
-    {
-        "no": "two",
-        "input_1": ListNode([]),
-        "input_2": ListNode([]),
-        "answer": ListNode([])
-    },
-    {
-        "no": "three",
-        "input_1": ListNode([]),
-        "input_2": ListNode([0]),
-        "answer": ListNode([0])
+        "input_1": createLinkedList([1, 2, 4]),
+        "input_2": createLinkedList([1, 3, 4]),
+        "answer": createLinkedList([1, 1, 2, 3, 4, 4])
     }
 ]
 
 for test in test_cases:
     result = sol.mergeTwoLists(test["input_1"], test["input_2"])
-    if result == test["answer"]:
-        print("answer is RIGHT")
-    else:
-        print("failed")
+    # if result == test["answer"]:
+    #     print("answer is RIGHT")
+    # else:
+    #     print("failed")
+
+
+backup = [
+    {
+        "no": "two",
+        "input_1": createLinkedList([]),
+        "input_2": createLinkedList([]),
+        "answer": createLinkedList([])
+    },
+    {
+        "no": "three",
+        "input_1": createLinkedList([]),
+        "input_2": createLinkedList([0]),
+        "answer": createLinkedList([0])
+    }
+]
