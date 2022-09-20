58898dee58b08ed60d26a3c9380c21dc1b3ed08a
HaddeeAndd <haddeeann@gmail.com>
2022-09-19 20:08:38 -0400

    somedays are the minimal effort

---
+++ leetcode/python/141/linked.py
@@ -0,0 +1,32 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = head.next
+        slow = head
+        while fast and fast.next:
+            slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
+
+
+def makeListNode(list_input):
+    h = None
+    for val in reversed(list_input):
+        h = ListNode(val, h)
+    return h
+
+
+l = makeListNode([1, 2])
+s = Solution()
+s.hasCycle(l)
