243014a02048b81e9328c0febf0106196a46c054
HaddeeAndd <haddeeann@gmail.com>
2022-09-21 20:10:59 -0400

    well. there you go. didn't work

--- leetcode/python/121/stocks.py
+++ leetcode/python/121/stocks.py
@@ -4,4 +4,19 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        pass
+        max_sell = prices[len(prices) - 1]
+        i_sell = len(prices) - 1
+        min_buy = prices[len(prices) - 1]
+        i_buy = len(prices) - 1
+        for i, p in enumerate(reversed(prices)):
+            if p > max_sell and i_sell < i_buy:
+                max_sell = p
+                i_sell = i
+            if p < min_buy:
+                min_buy = p
+                i_buy = i
+
+
+test = [7,1,5,3,6,4]
+s = Solution()
+s.maxProfit(test)
