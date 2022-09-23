189510c85df5b2726edfd10eb313e52209a6faad
HaddeeAndd <haddeeann@gmail.com>
2022-09-22 21:45:42 -0400

    doing something goofy. will look at it tomorrow

--- leetcode/python/121/stocks.py
+++ leetcode/python/121/stocks.py
@@ -4,17 +4,22 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        max_sell = prices[len(prices) - 1]
-        i_sell = len(prices) - 1
-        min_buy = prices[len(prices) - 1]
-        i_buy = len(prices) - 1
-        for i, p in enumerate(reversed(prices)):
-            if p > max_sell and i_sell < i_buy:
-                max_sell = p
-                i_sell = i
-            if p < min_buy:
-                min_buy = p
-                i_buy = i
+        highest_profit = None
+        # do it stupid first
+        for b, buy_day in enumerate(prices):
+            highest_SELL_day = None
+            buy_price = prices[b]
+            print(f"buy day is {b} ${buy_price}")
+            for s, sell_day in enumerate(prices[b+1:], start=b+1):
+                if not highest_SELL_day or prices[s] > highest_SELL_day:
+                    highest_SELL_day = prices[s]
+                    print(f"SELL day is: {s} ${prices[s]}")
+            profit = highest_SELL_day - buy_price
+            if not highest_profit or profit > highest_profit:
+                highest_profit = profit
+
+        print(hightest_profit)
+
 
 
 test = [7,1,5,3,6,4]
