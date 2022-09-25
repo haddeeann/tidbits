51547df6deb864c7af427d869d816bb9d4105d2e
HaddeeAndd <haddeeann@gmail.com>
2022-09-25 16:56:39 -0400

    need a cache for it, for next time

--- leetcode/python/121/stocks.py
+++ leetcode/python/121/stocks.py
@@ -4,24 +4,23 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        highest_profit = None
+        highest_profit = 0
         # do it stupid first
-        for b, buy_day in enumerate(prices):
-            highest_SELL_day = None
-            buy_price = prices[b]
+        for b, buy_price in enumerate(prices):
+            highest_price = buy_price
             print(f"buy day is {b} ${buy_price}")
-            for s, sell_day in enumerate(prices[b+1:], start=b+1):
-                if not highest_SELL_day or prices[s] > highest_SELL_day:
-                    highest_SELL_day = prices[s]
-                    print(f"SELL day is: {s} ${prices[s]}")
-            profit = highest_SELL_day - buy_price
-            if not highest_profit or profit > highest_profit:
+            for s, sell_price in enumerate(prices[b+1:], start=b+1):
+                if not highest_price or sell_price > highest_price:
+                    highest_price = sell_price
+                    print(f"SELL day is: {s} ${sell_price}")
+            profit = highest_price - buy_price
+            if profit > highest_profit:
                 highest_profit = profit
 
-        print(hightest_profit)
+        return highest_profit
 
 
 
 test = [7,1,5,3,6,4]
-s = Solution()
-s.maxProfit(test)
+sol = Solution()
+sol.maxProfit(test)
