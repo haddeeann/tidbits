05bc9c632e732f58819391ff2b4eed66a8e1ee35
HaddeeAndd <haddeeann@gmail.com>
2022-09-27 21:01:31 -0400

    slow and steady. worked on stocks, added test cases

--- leetcode/python/121/stocks.py
+++ leetcode/python/121/stocks.py
@@ -5,23 +5,38 @@ class Solution(object):
         :rtype: int
         """
         highest_profit = 0
-        # do it stupid first
+        highest_sell_price = prices[0]
         cache = None
         for b, buy_price in enumerate(prices):
-            highest_price = buy_price
             if not cache or b >= cache:
                 for s, sell_price in enumerate(prices[b+1:], start=b+1):
-                    if sell_price > highest_price:
+                    if sell_price > highest_sell_price:
                         cache = s
-                        highest_price = sell_price
-            profit = highest_price - buy_price
+                        highest_sell_price = sell_price
+            profit = highest_sell_price - buy_price
             if profit > highest_profit:
                 highest_profit = profit
 
         return highest_profit
 
 
+test_cases = [
+    {
+        "no": 1,
+        "input": [7,1,5,3,6,4],
+        "answer": 5
+    },
+    {
+        "no": 2,
+        "input": [2,1,4],
+        "answer": 3
+    }
+]
 
-test = [2,1,4]
 sol = Solution()
-sol.maxProfit(test)
+for x in test_cases:
+    answer = sol.maxProfit(x["input"])
+    if answer == x["answer"]:
+        print(f"answer {x['no']} is correct")
+    else:
+        print(f"answer {x['no']} is WRONG")
