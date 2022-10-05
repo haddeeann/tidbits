df5851e3cafdff4a16891b941be5545d3e4adee7
HaddeeAndd <haddeeann@gmail.com>
2022-10-04 21:43:01 -0400

    totally setting up for ksum and walking through a given solution to understand it.

---
+++ leetcode/python/18/four_sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        def kSum(nums: List[int], target: int, k: int):
+            pass
+
+        def twoSum(nums: List[int], target: int):
+            pass
+
+        nums.sort()
+        return kSum(nums, target, 4)
+
+sol = Solution()
+sol.fourSum([1,0,-1,0,-2,2], 0)
