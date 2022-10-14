db2999027a381ac4e70ae3e14debb5a5c0af6fd8
HaddeeAndd <haddeeann@gmail.com>
2022-10-13 20:58:34 -0400

    do I ever stop feeling stupid?

--- leetcode/python/19/node.py
+++ leetcode/python/19/node.py
@@ -12,11 +12,39 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        print(head, n)
+        count = 0
+        node = head
+        new_linked = []
+        while node:
+            count += 1
+            node = node.next
+
+        print(count)
+        node_remove = n - count
+        count = 0
+        while node:
+            new_linked.append(node.val)
+            count += 1
+            node = node.next
+            if count == node_remove:
+                skip
+        print(new_linked)
+
+
+def createLinkedList(values):
+    h = None
+    for val in reversed(values):
+        h = ListNode(val, h)
+    return h
 
 
 test_case = {
     "no": "one",
-    "input": ([1,2,3,4,5], 2),
-    "answer": [1,2,3,5],
+    "input": ([1, 2, 3, 4, 5], 2),
+    "answer": [1, 2, 3, 5],
 }
+
+s = Solution()
+c = [1, 2, 3, 4, 5]
+node_list = createLinkedList(c)
+s.removeNthFromEnd(node_list, 2)
