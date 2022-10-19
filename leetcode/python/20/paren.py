7bbdb54c3c52c7caae9f368084e44c13e2f01722
HaddeeAndd <haddeeann@gmail.com>
2022-10-19 19:56:15 -0400

    nope, that isn't even close to what I want to do

--- leetcode/python/20/paren.py
+++ leetcode/python/20/paren.py
@@ -4,3 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        start_set = {"(", "[", "{"}
+        end_set = {")", "]", "}"}
+        index = 0
+        for l in s:
+            if l in start_set and index != len(s) - 1:
+                if s[index+1] in end_set:
+                    index += 2
+            else:
+                index += 1
+
+
+
+s = Solution()
+s.isValid("()")
