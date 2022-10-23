55530a12c7c0ceb51384969ed392188d4e00d559
HaddeeAndd <haddeeann@gmail.com>
2022-10-23 19:23:42 -0400

    im not gonna sit here, I want what we had, just wanna feel it with someone else

--- leetcode/python/22/generate_paren.py
+++ leetcode/python/22/generate_paren.py
@@ -10,7 +10,7 @@ class Solution(object):
         a = ""
         pair = [l_box, r_box]
         for p in pair:
-            for i in range(n):
+            for i in range(3):
                 a += p
         print(a)
         b = ""
@@ -21,5 +21,24 @@ class Solution(object):
                     b += p
             b += r_box
         print(b)
+
+        c = ""
+        for p in pair:
+            for i in range(2):
+                c += p
+        for p in pair:
+            c += p
+        print(c)
+
+        d = ""
+        for p in pair:
+            d += p
+        for p in pair:
+            for i in range(2):
+                d += p
+        print(d)
+
+
+
 sol = Solution()
 sol.generateParenthesis(3)
