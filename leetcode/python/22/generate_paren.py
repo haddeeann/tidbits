6a001bd86820a8af9c64a257392d0ab5622fe037
HaddeeAndd <haddeeann@gmail.com>
2022-10-25 21:29:18 -0400

    add a bit of complexity to the problem

--- leetcode/python/22/generate_paren.py
+++ leetcode/python/22/generate_paren.py
@@ -18,6 +18,12 @@ class Solution(object):
                 str += p
         return str
 
+    def insertInner(self, outer, inner):
+        half = int(len(outer) / 2)
+        left = outer[:half]
+        right = outer[half:]
+        return left + inner + right
+
     def generateParenthesis(self, n):
         """
         :type n: int
@@ -27,6 +33,16 @@ class Solution(object):
         l_box = "("
         r_box = ")"
         pair = [l_box, r_box]
+        inner = n - 1
+        answer = []
+        while inner >= 2:
+            outer = n - inner
+
+            outer_paren = self.innerParen("", outer, pair)
+            inner_paren = self.sideBySide("", inner, pair)
+            e = self.insertInner(outer_paren, inner_paren)
+            answer.append(e)
+            inner -= 1
 
         right = 0
         while right < n:
@@ -36,11 +52,24 @@ class Solution(object):
 
             d = ""
             d = self.sideBySide(d, right, pair)
-            print(c + d)
+            answer.append(c + d)
             if right != 0 and left != 1:
-                print(d + c)
+                answer.append(d + c)
             right += 1
+        return answer
 
 
 sol = Solution()
-sol.generateParenthesis(5)
+guess = sol.generateParenthesis(4)
+print(guess)
+answer = ["(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()", "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"]
+print(answer)
+a_set = set()
+for a in answer:
+    found = False
+    for x in guess:
+        if a == x:
+            found = True
+    if not found:
+        a_set.add(a)
+print(a_set)
