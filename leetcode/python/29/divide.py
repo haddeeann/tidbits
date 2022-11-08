0690f0f25c7f56fd6bcc4f21686ce7aea3968149
HaddeeAndd <haddeeann@gmail.com>
2022-11-07 21:04:32 -0500

    me make pretty code

--- leetcode/python/29/divide.py
+++ leetcode/python/29/divide.py
@@ -1,58 +1,34 @@
 class Solution(object):
-    def find_a(self, dividend, divisor):
-        k = 0
-        a = 0
-        counter = divisor
-        if divisor % 2 == 0:
-            answer = dividend
-            while divisor > 0:
-                answer = answer >> 1
-                divisor -= 2
-            a = answer
-        else:
-            while counter < dividend:
-                a += 1
-                counter += divisor
-        print(a)
-        return a
-
-    def divide(self, dividend, divisor):
+    def divide(self, x, y):
         """
         :type dividend: int
         :type divisor: int
         :rtype: int
         """
-        # limits
-        neg_limit = -2**31
-        pos_limit = 2**31 - 1
-        # negative tester
-        neg_divisor = divisor < 0
-        neg_dividend = dividend < 0
-        # set limits
-        if dividend > pos_limit:
-            dividend = pos_limit
-        elif dividend < neg_limit:
-            dividend = neg_limit
-        # get absolute values
-        divisor = abs(divisor)
-        dividend = abs(dividend)
-        # anything divided by itself is 1
-        if divisor == dividend:
-            a = 1
-        elif divisor == 1:
-            a = dividend
-        else:
-            a = self.find_a(dividend, divisor)
-        if (neg_divisor and not neg_dividend) or (not neg_divisor and neg_dividend):
-            a = -abs(a)
-        if a > pos_limit:
-            a = pos_limit
-        elif a < neg_limit:
-            a = neg_limit
-        print(a)
-        return a
+        ans = 0
+        min = -2**31
+        max = 2**31-1
+        xx, yy = abs(x), abs(y)
+        for i in range(32, -1, -1):
+            if xx >= (yy << i):
+                subtract = (yy << i)
+                xx -= subtract
+                add = (1 << i)
+                ans += add
+
+        x_neg = x < 0
+        y_neg = y < 0
+        if (x_neg and not y_neg) or (y_neg and not x_neg):
+            ans = -ans
+
+        if ans < min:
+            return min
+        elif ans > max:
+            return max
+
+        return ans
 
 
 sol = Solution()
-# sol.find_a(2147483647, 2)
-sol.find_a(12, 4)
+
+sol.divide(112, 3)
