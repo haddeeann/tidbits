a24573b418ea7d5ec03d381209336689f7089c8e
HaddeeAndd <haddeeann@gmail.com>
2022-11-12 20:52:33 -0500

    a little bit at a time

--- leetcode/python/30/cat.py
+++ leetcode/python/30/cat.py
@@ -5,18 +5,33 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
-        bar_list = []
+        found_dict = {}
         bar = words[0]
-        found = 0
-        index = 0
-        start = 0
+        count = 0
         l = len(words[0])
-        while found != -1:
-            found = s.find(bar, start)
-            if found != -1:
-                bar_list.append(found)
-                print(found)
-                start = found + l - 1
+        for w in words:
+            print(w)
+            found = 0
+            start = 0
+            while found != -1:
+                count += 1
+                found = s.find(w, start)
+                if found != -1:
+                    found_dict[found] = w
+                    start = found + l - 1
+        print('count ', count)
+        print('found ', found_dict)
+
+        for index in found_dict:
+            s_set = set(words)
+            step = index
+            while len(s_set) > 0:
+                if step in found_dict:
+                    word = found_dict[step]
+                    s_set.remove(word)
+                    word_len = len(found_dict[index])
+                    step = step + word_length
+                    print(step, word, word_len)
 
 
 sol = Solution()
