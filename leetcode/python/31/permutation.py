0cc84664da4041fb1e817c0dacd8e16ff191a28c
HaddeeAndd <haddeeann@gmail.com>
2022-11-28 20:29:54 -0500

    permutations

---
+++ leetcode/python/31/permutation.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        print(nums)
+
+sol = Solution()
+sol([1,2,3])
