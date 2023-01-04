056b3706eeebaafa35f9bbaa6507b1aab9eeac7b
HaddeeAndd <haddeeann@gmail.com>
2023-01-03 20:49:54 -0500

    started sudoku

--- leetcode/python/36/sudoku.py
+++ leetcode/python/36/sudoku.py
@@ -1,17 +1,44 @@
 class Solution(object):
+    def testRow(self, row):
+        row_list = []
+        for r in row:
+            if r in row_list and r != ".":
+                return False
+            elif r != ".":
+                row_list.append(r)
+        return True
+
+
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: bool
         """
-        print(board)
+        columns = []
+        for row in board:
+            row_truth = self.testRow(row)
 
+        for column in columns:
+            print('hi')
 
+board_a = [["5","3","5",".","7",".",".",".","."],
+         ["6",".",".","1","9","5",".",".","."],
+         [".","9","8",".",".",".",".","6","."],
+         ["8",".",".",".","6",".",".",".","3"],
+         ["4",".",".","8",".","3",".",".","1"],
+         ["7",".",".",".","2",".",".",".","6"],
+         [".","6",".",".",".",".","2","8","."],
+         [".",".",".","4","1","9",".",".","5"],
+         [".",".",".",".","8",".",".","7","9"]]
 tests = [{
-    "input": "",
-    "answer": "",
+    "input": board_a,
+    "answer": True,
 }]
 
 sol = Solution()
 for t in tests:
-    sol.isValidSudoku(t["input"])
+    answer = sol.isValidSudoku(t["input"])
+    if answer == t["answer"]:
+        print("passed the test")
+    else:
+        print("did not pass the test")
