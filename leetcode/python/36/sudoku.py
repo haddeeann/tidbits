e0cec58d7bc56ed68a3c907e6efa84431a747775
HaddeeAndd <haddeeann@gmail.com>
2023-01-04 20:53:52 -0500

    we learn the lessons until we know them. Lists are created by reference unless a copy is made

--- leetcode/python/36/sudoku.py
+++ leetcode/python/36/sudoku.py
@@ -1,35 +1,28 @@
 class Solution(object):
-    def testRow(self, row):
-        row_list = []
-        for r in row:
-            if r in row_list and r != ".":
-                return False
-            elif r != ".":
-                row_list.append(r)
-        return True
-
-
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: bool
         """
         columns = []
-        for row in board:
-            row_truth = self.testRow(row)
-
-        for column in columns:
-            print('hi')
-
-board_a = [["5","3","5",".","7",".",".",".","."],
-         ["6",".",".","1","9","5",".",".","."],
-         [".","9","8",".",".",".",".","6","."],
-         ["8",".",".",".","6",".",".",".","3"],
-         ["4",".",".","8",".","3",".",".","1"],
-         ["7",".",".",".","2",".",".",".","6"],
-         [".","6",".",".",".",".","2","8","."],
-         [".",".",".","4","1","9",".",".","5"],
-         [".",".",".",".","8",".",".","7","9"]]
+        letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+        for r in range(9):
+            columns.append(letters.copy())
+        for i, row in enumerate(board):
+            for j, col in enumerate(row):
+                columns[j][i] = col
+        print(columns)
+board_a = [
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
 tests = [{
     "input": board_a,
     "answer": True,
