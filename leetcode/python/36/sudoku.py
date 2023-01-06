8a916556c885a9e695dec26d9d820aefc1155dd8
HaddeeAndd <haddeeann@gmail.com>
2023-01-05 21:22:11 -0500

    tested the sudoku, next it's solving

--- leetcode/python/36/sudoku.py
+++ leetcode/python/36/sudoku.py
@@ -5,13 +5,69 @@ class Solution(object):
         :rtype: bool
         """
         columns = []
-        letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+        grids = []
         for r in range(9):
-            columns.append(letters.copy())
+            columns.append([])
+            grids.append([])
         for i, row in enumerate(board):
+            add_row = []
             for j, col in enumerate(row):
-                columns[j][i] = col
-        print(columns)
+                if col != ".":
+                    if col in add_row or col in columns[j]:
+                        return False
+
+                    add_row.append(col)
+                    columns[j].append(col)
+                    if j < 3:
+                        if i < 3:
+                            if col in grids[0]:
+                                return False
+                            else:
+                                grids[0].append(col)
+                        elif i < 6:
+                            if col in grids[1]:
+                                return False
+                            else:
+                                grids[1].append(col)
+                        elif i < 9:
+                            if col in grids[2]:
+                                return False
+                            else:
+                                grids[2].append(col)
+                    elif j < 6:
+                        if i < 3:
+                            if col in grids[3]:
+                                return False
+                            else:
+                                grids[3].append(col)
+                        elif i < 6:
+                            if col in grids[4]:
+                                return False
+                            else:
+                                grids[4].append(col)
+                        elif i < 9:
+                            if col in grids[5]:
+                                return False
+                            else:
+                                grids[5].append(col)
+                    elif j < 9:
+                        if i < 3:
+                            if col in grids[6]:
+                                return False
+                            else:
+                                grids[6].append(col)
+                        elif i < 6:
+                            if col in grids[7]:
+                                return False
+                            else:
+                                grids[7].append(col)
+                        elif i < 9:
+                            if col in grids[8]:
+                                return False
+                            else:
+                                grids[8].append(col)
+        return True
+
 board_a = [
     ["5","3",".",".","7",".",".",".","."],
     ["6",".",".","1","9","5",".",".","."],
