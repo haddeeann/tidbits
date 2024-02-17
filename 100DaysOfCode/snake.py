ce4558bde66dcf72283766b27b35bd024340e22a
HaddeeAndd <haddeeann@gmail.com>
2024-02-16 19:22:40 -0500

    dont be able to go backwards

--- 100DaysOfCode/snake.py
+++ 100DaysOfCode/snake.py
@@ -2,12 +2,18 @@ import turtle
 
 
 class Snake:
+    UP = 90
+    DOWN = 270
+    LEFT = 180
+    RIGHT = 0
+
     def __init__(self):
         self.segments = []
         starting_positions = [(0, 0), (-20, 0), (-40, 0)]
         for position in starting_positions:
             square = self.draw_square(position)
             self.segments.append(square)
+        self.head = self.segments[0]
 
     def draw_square(self, position):
         square = turtle.Turtle()
@@ -23,16 +29,20 @@ class Snake:
             new_y = self.segments[seg - 1].ycor()
             self.segments[seg].speed(1)
             self.segments[seg].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(20)
 
     def up(self):
-        self.segments[0].setheading(90)
+        if self.head.heading() != self.DOWN:
+            self.head.setheading(self.UP)
 
     def down(self):
-        self.segments[0].setheading(270)
+        if self.head.heading() != self.UP:
+            self.head.setheading(self.DOWN)
 
     def right(self):
-        self.segments[0].setheading(0)
+        if self.head.heading() != self.LEFT:
+            self.head.setheading(self.RIGHT)
 
     def left(self):
-        self.segments[0].setheading(180)
+        if self.head.heading() != self.RIGHT:
+            self.segments[0].setheading(self.LEFT)
