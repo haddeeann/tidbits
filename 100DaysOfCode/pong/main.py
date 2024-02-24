377bb0c21e99f84b31620f40a88f4cff1f0408bf
HaddeeAndd <haddeeann@gmail.com>
2024-02-23 19:28:22 -0500

    paddle made

--- 100DaysOfCode/pong/main.py
+++ 100DaysOfCode/pong/main.py
@@ -1,9 +1,35 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=1000, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
+paddle = Turtle()
+paddle.shape('square')
+paddle.color('white')
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, 'Up')
+screen.onkey(go_down, 'Down')
+
+game_on = True
+while game_on:
+    screen.update()
 
 screen.exitonclick()
