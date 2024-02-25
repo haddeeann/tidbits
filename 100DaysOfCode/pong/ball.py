dd0ef0bc19236d8160461acf9fac9d03de15db81
HaddeeAndd <haddeeann@gmail.com>
2024-02-25 12:27:29 -0500

    some real bounce to the walls

--- 100DaysOfCode/pong/ball.py
+++ 100DaysOfCode/pong/ball.py
@@ -8,7 +8,7 @@ class Ball(Turtle):
         self.shape('circle')
         self.penup()
         self.goto(0,0)
-        self.setheading(180)
+        self.setheading(170)
 
     def move(self):
         WIDTH = 1000
@@ -21,17 +21,51 @@ class Ball(Turtle):
         UP = 90
         LEFT = 180
         DOWN = 270
-        self.forward(2)
-        print(int(self.xcor()), int(self.ycor()))
+        self.forward(20)
         if int(self.xcor()) >= right_edge:
-            self.setheading(LEFT)
+            if self.heading() == RIGHT:
+                # before heading RIGHT 0
+                # after heading LEFT 180
+                self.setheading(LEFT)
+            else:
+                heading = int(self.heading())
+                print(heading)
+                if heading < UP:
+                    self.setheading(heading+90)
+                elif heading > DOWN:
+                    self.setheading(heading+180)
 
         elif int(self.xcor()) <= left_edge:
-            self.setheading(RIGHT)
+            if self.heading() == LEFT:
+                # before heading LEFT 180
+                # after heading RIGHT 0
+                self.setheading(RIGHT)
+            else:
+                heading = int(self.heading())
+                if heading < LEFT:
+                    self.setheading(heading-90)
+                elif heading > LEFT:
+                    self.setheading(heading+90)
 
         elif int(self.ycor()) >= up_edge:
-            self.setheading(DOWN)
-
+            if self.heading() == UP:
+                # before heading UP 90
+                # after heading DOWN 270
+                self.setheading(DOWN)
+            else:
+                heading = int(self.heading())
+                if heading < UP:
+                    self.setheading(360-heading)
+                elif heading > UP:
+                    self.setheading(90+heading)
         elif int(self.ycor()) <= bottom_edge:
-            self.setheading(UP)
-
+            if self.heading() == DOWN:
+                # before heading DOWN 270
+                # after heading UP 90
+                self.setheading(UP)
+            else:
+                heading = int(self.heading())
+                if heading > DOWN:
+                    self.setheading(360-heading)
+                elif heading < DOWN:
+                    self.setheading(heading-90)
