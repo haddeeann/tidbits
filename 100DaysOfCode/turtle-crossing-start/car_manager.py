00b47e4a375945fdad1bece166f5642bf9517cda
HaddeeAndd <haddeeann@gmail.com>
2024-03-05 21:07:51 -0500

    yeah! continual cars

--- 100DaysOfCode/turtle-crossing-start/car_manager.py
+++ 100DaysOfCode/turtle-crossing-start/car_manager.py
@@ -11,14 +11,16 @@ class CarManager:
     def __init__(self):
         super().__init__()
         self.cars = []
-        self.make_batch()
 
     def batch_manager(self):
+        self.make_batch()
         self.move()
 
     def make_batch(self):
-        for car_num in range(6):
-            self.make_car(car_num)
+        random_chance = random.randint(1, 32)
+        if random_chance == 1:
+            for car_num in range(6):
+                self.make_car(car_num)
 
     def make_car(self, car_num):
         color_index = car_num
@@ -27,7 +29,7 @@ class CarManager:
         car.shape('square')
         car.shapesize(stretch_wid=1, stretch_len=2)
         car.penup()
-        x = random.randint(-400, 400)
+        x = random.randint(300, 400)
         y = random.randint(-400, 400)
         car.goto(x, y)
         self.cars.append(car)
