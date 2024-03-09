512ce69e62e4dc388f121de46b3b1d7cfefd6580
HaddeeAndd <haddeeann@gmail.com>
2024-03-09 10:13:05 -0500

    scoreboard for turtle put in place

--- 100DaysOfCode/turtle-crossing-start/scoreboard.py
+++ 100DaysOfCode/turtle-crossing-start/scoreboard.py
@@ -4,7 +4,20 @@ FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
-        pass
+        super().__init__()
+        self.color('blue')
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-200, 250)
+        self.score = f"Level: {self.level}"
+        self.write(self.score, font=FONT)
 
     def level_up(self):
         print('level up')
+        self.level += 1
+        self.update_scoreboard()
