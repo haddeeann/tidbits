0423f81135623e368c66e7c0d743a3bb5a0a55a2
HaddeeAndd <haddeeann@gmail.com>
2024-03-11 21:17:35 -0400

    high score for snake game, in file form

--- 100DaysOfCode/snake/scoreboard.py
+++ 100DaysOfCode/snake/scoreboard.py
@@ -8,7 +8,12 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('highscore.txt', mode='r') as file:
+            score = file.read()
+            if score:
+                self.high_score = int(score)
+            else:
+                self.high_score = 0
         self.color('white')
         self.penup()
         self.goto(0, 270)
@@ -25,6 +30,8 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
+            with open('highscore.txt', mode='w') as file:
+                file.write(str(self.score))
             self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
