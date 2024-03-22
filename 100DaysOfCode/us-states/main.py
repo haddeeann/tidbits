2197b33bed802b1fe9b73407bfb725c1f0e631fb
HaddeeAndd <haddeeann@gmail.com>
2024-03-22 08:30:31 -0400

    states games does the screen add the states names without erroring.

--- 100DaysOfCode/us-states/main.py
+++ 100DaysOfCode/us-states/main.py
@@ -1,16 +1,31 @@
 import turtle
 import pandas
 
+ALIGN = 'center'
+FONT = ('Arial', 10, 'normal')
+
 screen = turtle.Screen()
 screen.title("US States Game")
 
 image = 'blank_states_img.gif'
-states = './50_states.csv'
+file = './50_states.csv'
 screen.addshape(image)
-data = pandas.read_csv(states)
-print(data)
+data = pandas.read_csv(file)
+all_states = data.state.to_list()
+
 turtle.shape(image)
-
-answer_state = screen.textinput(title='Guess the state', prompt='what is another state?')
-
+state_len = 50
+while state_len > 0:
+    answer = screen.textinput(title='Guess the state', prompt='what is another state?')
+    if answer.lower() in (state.lower() for state in all_states):
+        row = data[data.state == answer]
+        if not row.empty:
+            x = row.x.iloc[0]
+            y = row.y.iloc[0]
+            t = turtle.Turtle()
+            t.hideturtle()
+            t.color('blue')
+            t.penup()
+            t.goto(x, y)
+            t.write(row.state.item(), align=ALIGN, font=FONT)
 turtle.mainloop()
