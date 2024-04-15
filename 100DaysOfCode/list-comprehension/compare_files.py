a605ad6cc6aaecb92c3b9bcd659d4e8b29990ae6
HaddeeAndd <haddeeann@gmail.com>
2024-04-15 11:06:29 -0400

    we can do it in one line instead of two lines

--- 100DaysOfCode/list-comprehension/compare_files.py
+++ 100DaysOfCode/list-comprehension/compare_files.py
@@ -1,12 +1,9 @@
 with open('./numbers1.txt', 'r') as f:
-    file1 = f.readlines()
+    numbers1 = f.readlines()
 
 with open('./numbers2.txt', 'r') as f:
-    file2 = f.readlines()
+    numbers2 = f.readlines()
 
-numbers1 = [int(n) for n in file1]
-numbers2 = [int(n) for n in file2]
-
-overlap = [n for n in numbers1 if n in numbers2]
+overlap = [int(n) for n in numbers1 if n in numbers2]
 
 print(overlap)
