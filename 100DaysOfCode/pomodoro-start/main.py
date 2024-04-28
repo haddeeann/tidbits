f9e0a4afaac010e602679f9c77e03578af3efe0e
HaddeeAndd <haddeeann@gmail.com>
2024-04-28 13:07:29 -0400

    pomodoro start, added UI for button, label, checkmarks

--- 100DaysOfCode/pomodoro-start/main.py
+++ 100DaysOfCode/pomodoro-start/main.py
@@ -18,11 +18,25 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+label = Label(text='Timer', bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35))
+label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.pack()
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+
+canvas.grid(column=1, row=1)
+
+start = Button(text='Start', bg=YELLOW)
+start.grid(column=0, row=2)
+
+reset = Button(text='Reset', bg=YELLOW)
+reset.grid(column=2, row=2)
+
+check = Label(text='✔', bg=YELLOW, fg=GREEN)
+check.grid(column=1, row=3)
 
 window.mainloop()
