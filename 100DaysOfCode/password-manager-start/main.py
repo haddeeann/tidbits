e4fdb94825fd41253a4f6fc30937087ee0b56cec
HaddeeAndd <haddeeann@gmail.com>
2024-05-08 20:56:53 -0400

    fix the first pass on putting the grid together

--- 100DaysOfCode/password-manager-start/main.py
+++ 100DaysOfCode/password-manager-start/main.py
@@ -16,9 +16,46 @@ window.title('Password Manager')
 window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=224, highlightthickness=0)
-tomato_img = PhotoImage(file="logo.png")
+tomato_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=tomato_img)
 
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
+
+# website input/label
+website_label = Label(text='Website')
+website_label.grid(column=0, row=1)
+
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+# email input/label
+email_label = Label(text='Email/Username')
+email_label.grid(column=0, row=2)
+
+email_input = Entry(width=35)
+email_input.grid(column=1, row=2, columnspan=2)
+
+# password input/label
+password_label = Label(text='Password')
+password_label.grid(column=0, row=3)
+
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
+
+
+def generate_password():
+    pass
+
+
+button = Button(text='Generate Password', command=generate_password)
+button.grid(column=2, row=3)
+
+
+def add_password():
+    pass
+
+
+button = Button(text='Add', command=add_password, width=36)
+button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
