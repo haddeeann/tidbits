d73ad6f3db58f5441eb13422c635c1ed05220828
HaddeeAndd <haddeeann@gmail.com>
2024-05-08 21:01:28 -0400

    tweak the size

--- 100DaysOfCode/password-manager-start/main.py
+++ 100DaysOfCode/password-manager-start/main.py
@@ -25,21 +25,21 @@ canvas.grid(column=1, row=0)
 website_label = Label(text='Website')
 website_label.grid(column=0, row=1)
 
-website_input = Entry(width=35)
+website_input = Entry(width=38, justify='left')
 website_input.grid(column=1, row=1, columnspan=2)
 
 # email input/label
 email_label = Label(text='Email/Username')
 email_label.grid(column=0, row=2)
 
-email_input = Entry(width=35)
+email_input = Entry(width=38, justify='left')
 email_input.grid(column=1, row=2, columnspan=2)
 
 # password input/label
 password_label = Label(text='Password')
 password_label.grid(column=0, row=3)
 
-password_input = Entry(width=21)
+password_input = Entry(width=21, justify='left')
 password_input.grid(column=1, row=3)
 
 
