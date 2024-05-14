7562e33c776cc8128bb5d53062d6f9a20874d002
HaddeeAndd <haddeeann@gmail.com>
2024-05-14 06:31:27 -0400

    letter checking, put in a keyerror check for foxtrot

--- 100DaysOfCode/dict/foxtrot.py
+++ 100DaysOfCode/dict/foxtrot.py
@@ -2,8 +2,18 @@ import pandas
 
 df = pandas.read_csv('./nato_phonetic_alphabet.csv')
 
-nato = {row.letter:row.code for (index, row) in df.iterrows()}
+nato = {row.letter: row.code for (index, row) in df.iterrows()}
 
-word = input()
-word_list = [nato[letter.upper()] for letter in word]
-print(word_list)
+
+def input_cycle():
+    word = input('Enter a word: ')
+    try:
+        word_list = [nato[letter.upper()] for letter in word]
+    except KeyError as e:
+        print('You may only enter letters, please try again.\n')
+        input_cycle()
+    else:
+        print(word_list)
+
+
+input_cycle()
