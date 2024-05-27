fb275cade2e8368fe77f0bc8fb56fe50c992715c
HaddeeAndd <haddeeann@gmail.com>
2024-05-27 14:01:12 -0400

    add spanish word on the card

--- 100DaysOfCode/flashcards/main.py
+++ 100DaysOfCode/flashcards/main.py
@@ -4,11 +4,25 @@ window = Tk()
 window.title('Flashcards')
 bg_color = 'lightblue'
 window.config(bg=bg_color, padx=50, pady=50)
+FONT_NAME = "Courier"
 
-card_front = Canvas(width=850, height=550, highlightthickness=0, bg=bg_color)
+pairs = {
+    "hola": "hi",
+    "feliz": "happy"
+}
+
+first_spanish = ''
+first_english = ''
+for index, spanish in enumerate(pairs):
+    print(index, spanish, pairs[spanish])
+    first_spanish = spanish
+    first_english = pairs[spanish]
+
+card_canvas = Canvas(width=850, height=550, highlightthickness=0, bg=bg_color)
 card_front_img = PhotoImage(file='./images/card_front.png')
-card_front.create_image(425, 275, image=card_front_img)
-card_front.grid(column=0, row=0, rowspan=3)
+card_canvas.create_image(425, 275, image=card_front_img)
+card_canvas.create_text(425, 250, text=first_spanish, fill='black', font=(FONT_NAME, 45, 'bold'))
+card_canvas.grid(column=0, row=0, rowspan=3)
 
 forward = Canvas(width=100, height=100, highlightthickness=0, bg=bg_color)
 forward_img = PhotoImage(file='./images/next_small.png')
