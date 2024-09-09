8d3ed79fe17f23d0c136d7c83b0e6eb8a7de8f5f
HaddeeAndd <haddeeann@gmail.com>
2024-09-08 20:36:06 -0400

    swtich up images. Not great, just ok

--- 100DaysOfCode/flashcards/main.py
+++ 100DaysOfCode/flashcards/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import pandas
+from PIL import Image, ImageTk
 
 df = pandas.read_csv('./data/french_words.csv')
 french_dict = df.to_dict('records')
@@ -14,6 +15,8 @@ FONT_NAME = "Courier"
 first_english = ''
 language = "French"
 card_number = 0
+
+# card canvas setup
 card_canvas = Canvas(width=850, height=550, highlightthickness=0, bg=bg_color)
 card_front_img = PhotoImage(file='./images/card_front.png')
 card_canvas.create_image(425, 275, image=card_front_img)
@@ -44,13 +47,29 @@ def previous_card():
     text_id = card_canvas.create_text(425, 250, text=french_dict[card_number][language], fill='black',
                                       font=(FONT_NAME, 45, 'bold'))
 
+# Load and resize the image using Pillow
+next_image_lg = Image.open('./images/next.png')
+resized_image = next_image_lg.resize((50, 50), Image.ANTIALIAS)  # Resize the image to 50x50
+next_img = ImageTk.PhotoImage(resized_image)
 
-img = PhotoImage(file='./images/next_small.png')
+# Create the button with the resized image
 Button(
     window,
-    image=img,
-    command=next_card
+    image=next_img,
+    command=next_card,
 ).grid(column=1, row=0)
+
+# back button
+back_image_lg = Image.open('./images/back.png')
+resized_image = back_image_lg.resize((50, 50), Image.ANTIALIAS)  # Resize the image to 50x50
+back_img = ImageTk.PhotoImage(resized_image)
+
+# Create the button with the resized image
+Button(
+    window,
+    image=back_img,
+    command=previous_card
+).grid(column=1, row=1)
 
 
 def flip_card():
@@ -67,14 +86,21 @@ def flip_card():
         language = "French"
 
 
+# flip button
+flip_image_lg = Image.open('./images/flip.png')
+resized_image = flip_image_lg.resize((50, 50), Image.ANTIALIAS)  # Resize the image to 50x50
+flip_img = ImageTk.PhotoImage(resized_image)
 
-flip_img = PhotoImage(file='./images/flip.png')
-flip_button = Button(width=100, height=100, image=flip_img, bg=bg_color, command=flip_card)
-flip_button.grid(column=1, row=1)
+flip_button = Button(width=50, height=50, image=flip_img, bg=bg_color, command=flip_card)
+flip_button.grid(column=1, row=2)
 
-trash_canvas = Canvas(width=100, height=100, highlightthickness=0, bg=bg_color)
-trash_img = PhotoImage(file='./images/trash.png')
+# trash can
+trash_canvas = Canvas(width=50, height=50, highlightthickness=0, bg=bg_color)
+trash_image_lg = Image.open('./images/trash.png')
+resized_image = trash_image_lg.resize((50, 50), Image.ANTIALIAS)  # Resize the image to 50x50
+trash_img = ImageTk.PhotoImage(resized_image)
+
 trash_canvas.create_image(50, 50, image=trash_img)
-trash_canvas.grid(column=1, row=2)
+trash_canvas.grid(column=1, row=3)
 
 window.mainloop()
