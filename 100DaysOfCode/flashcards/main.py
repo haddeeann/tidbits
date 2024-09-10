d8e9e42666c688388b5bbc7fc41e83619e4055e4
HaddeeAndd <haddeeann@gmail.com>
2024-09-09 20:13:17 -0400

    fix trash image to fit

--- 100DaysOfCode/flashcards/main.py
+++ 100DaysOfCode/flashcards/main.py
@@ -15,13 +15,14 @@ FONT_NAME = "Courier"
 first_english = ''
 language = "French"
 card_number = 0
+button_column = 2
 
 # card canvas setup
 card_canvas = Canvas(width=850, height=550, highlightthickness=0, bg=bg_color)
 card_front_img = PhotoImage(file='./images/card_front.png')
 card_canvas.create_image(425, 275, image=card_front_img)
 text_id = card_canvas.create_text(425, 250, text=french_dict[card_number][language], fill='black', font=(FONT_NAME, 45, 'bold'))
-card_canvas.grid(column=0, row=0, rowspan=3)
+card_canvas.grid(column=1, row=1, rowspan=2)
 
 def next_card():
     global text_id
@@ -57,7 +58,7 @@ Button(
     window,
     image=next_img,
     command=next_card,
-).grid(column=1, row=0)
+).grid(column=button_column, row=0)
 
 # back button
 back_image_lg = Image.open('./images/back.png')
@@ -69,7 +70,7 @@ Button(
     window,
     image=back_img,
     command=previous_card
-).grid(column=1, row=1)
+).grid(column=button_column, row=1)
 
 
 def flip_card():
@@ -92,15 +93,17 @@ resized_image = flip_image_lg.resize((50, 50), Image.ANTIALIAS)  # Resize the im
 flip_img = ImageTk.PhotoImage(resized_image)
 
 flip_button = Button(width=50, height=50, image=flip_img, bg=bg_color, command=flip_card)
-flip_button.grid(column=1, row=2)
+flip_button.grid(column=button_column, row=2)
+
+def trash_card():
+    pass
 
 # trash can
-trash_canvas = Canvas(width=50, height=50, highlightthickness=0, bg=bg_color)
 trash_image_lg = Image.open('./images/trash.png')
 resized_image = trash_image_lg.resize((50, 50), Image.ANTIALIAS)  # Resize the image to 50x50
 trash_img = ImageTk.PhotoImage(resized_image)
 
-trash_canvas.create_image(50, 50, image=trash_img)
-trash_canvas.grid(column=1, row=3)
+trash_button = Button(width=50, height=50, image=trash_img, bg=bg_color, command=trash_card)
+trash_button.grid(column=button_column, row=3)
 
 window.mainloop()
